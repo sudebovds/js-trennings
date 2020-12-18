b0af26db7b6915614065ccefb880f3610800673b
Mit Sudebov <sudebovds@gmail.com>
2020-12-18 20:04:29 +0300

    Core types of typescript

---
+++ TypeScript Crush Course/2.Common/index.ts
@@ -0,0 +1,11 @@
+//Core TypeScript types
+
+const num: number = 0;
+const str: string = 'String constant';
+const bool: boolean = false;
+
+console.group('Typescript types:');
+    console.log(`Number: ${num}.`);
+    console.log(`String: ${str}.`);
+    console.log(`Boolean: ${bool}.`);
+console.groupEnd();
