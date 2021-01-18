1cfe1b6337f598c6085a233e6fc4078e5dd6d9c6
Mit Sudebov <sudebovds@gmail.com>
2021-01-18 19:05:32 +0300

    Initial settings. Base texturing. Save world state. Handlers for work with state

---
+++ Minecraft/src/hooks/useInterval.js
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from "react"
+
+export const useInterval = (callback, delay) => {
+    const savedCallback = useRef();
+
+    //Remenber the latest callback;
+
+    useEffect(() => {
+        savedCallback.current = callback;
+    });
+
+    //Setup the interval;
+
+    useEffect(() => {
+        function tick() {
+            if(typeof savedCallback?.current !== 'undefined'){
+                savedCallback.current();
+            }
+            if(delay !== null){
+                const id = setInterval(tick, delay);
+                return () => clearInterval(id)
+            }
+        }
+    }, [delay]);
+}
