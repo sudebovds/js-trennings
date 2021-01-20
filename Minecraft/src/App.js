4339e696b2b4dd96be84bf4cf1499c1eeb28085d
Mit Sudebov <d.sudebov@telesputnik.ru>
2021-01-20 19:23:30 +0300

    Small changes

--- Minecraft/src/App.js
+++ Minecraft/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import { Canvas } from 'react-three-fiber';
-import { Sky } from 'drei';
-import { Physics } from 'use-cannon';
+import React from "react";
+import { Canvas } from "react-three-fiber";
+import { Sky } from "drei";
+import { Physics } from "use-cannon";
 
-import { Ground } from './components/Ground';
-import { Player } from './components/Player';
-import { Cube } from './components/Cube';
-
-
+import { Ground } from "./components/Ground";
+import { Player } from "./components/Player";
+import { Cube } from "./components/Cube";
 
 function App() {
   return (
     <Canvas shadowMap sRGB>
-      <Sky sunPosition = {[100, 20, 100]} />
-      <ambientLight intensity = {0.25} />
-      <pointLight castShadow intensity = {0.7} position = {[100, 100, 100]} />
-      <Physics gravity = {[0, -30, 0]}>
-        <Ground position = {[0, 0.5, 0]} />
-        <Player position = {[0, 3, 10]} />
-        <Cube position = {[0, 1, 0]} type = 'wood' />
+      <Sky sunPosition={[100, 20, 100]} />
+      <ambientLight intensity={0.25} />
+      <pointLight castShadow intensity={0.7} position={[100, 100, 100]} />
+      <Physics gravity={[0, -30, 0]}>
+        <Ground position={[0, 0.5, 0]} />
+        <Player position={[0, 3, 10]} />
+        <Cube position={[0, 1, 0]} type="glass" />
       </Physics>
     </Canvas>
   );
