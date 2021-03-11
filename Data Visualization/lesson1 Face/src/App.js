67dcbda53cb6a2187669ee8caa60d477bb42a3b7
Mit Sudebov <sudebovds@gmail.com>
2021-03-11 21:29:52 +0300

    Devaided code on components. Added D3 arc. Refactoring

--- Data Visualization/lesson1 Face/src/App.js
+++ Data Visualization/lesson1 Face/src/App.js
@@ -1,32 +1,42 @@
 import React from 'react';
+import { Face } from './components/face/Face';
+import { Eye } from './components/eye/Eye';
+import { Mouth } from './components/mouth/Mouth';
 
 const App = () => {
-
+  
   const width = 960;
   const height = 500;
   const centerX = width / 2;
   const centerY = height / 2;
+  const strokeWidth = 2;
+  const radius = height / 2 - strokeWidth;
+  const eyeOffsetX = 120;
+  const eyeOffsetY = 100;
+  const eyeRadius = 50;
 
   return (
     <div className="face">
             <svg width={width} height={height} className="face__draw">
-                <circle 
-                    cx={centerX} 
-                    cy={centerY} 
-                    r="249"
+              <g transform={`translate(${centerX}, ${centerY})`}>
+                <Face 
+                    r={radius}
                     fill="yellow"
                     stroke="black"
-                ></circle>
-                <circle 
-                    cx="350" 
-                    cy="180" 
-                    r="50"
-                ></circle>
-                <circle 
-                    cx="600" 
-                    cy="180" 
-                    r="50"
-                ></circle>
+                    strokeWidth={strokeWidth}
+                />
+                <Eye
+                  centerX = {- eyeOffsetX} 
+                  centerY = {- eyeOffsetY}
+                  radius = {eyeRadius}
+                />
+                <Eye
+                  centerX = {eyeOffsetX} 
+                  centerY = {- eyeOffsetY}
+                  radius = {eyeRadius}
+                /> 
+                <Mouth />   
+              </g>            
             </svg>      
     </div>
   );
