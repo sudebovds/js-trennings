088f48dd9180f45b2add7282425a01b45e6035a0
Mit Sudebov <sudebovds@gmail.com>
2021-04-16 19:21:37 +0300

    State, styling

--- Data Visualization/lesson2 Covid19/reactData/src/Data.js
+++ Data Visualization/lesson2 Covid19/reactData/src/Data.js
@@ -1,27 +1,51 @@
-import { useState } from "react";
-import * as d3 from "d3";
+import { useEffect, useState } from "react";
+import { csv } from 'd3';
+import Message from "./components/message/Message";
 
-/*const width = 960;
-const height = 500;
-const circleRadius = 30;*/
 const url = `https://gist.githubusercontent.com/sudebovds/0620df3655bfe3638db025cd5e47da50/raw/cssColorsData.csv`;
+
+const liStyle = {
+    listStyle: 'none', 
+    cursor: 'pointer',
+    display: 'flex',
+    flexWrap: 'wrap',
+    maxWidth: '200px',
+    padding: '15px 25px'
+};
+
+const liClickHandler = (event) => {
+    document.getElementById('body').setAttribute('style', `background-color: ${event.target.id}`)
+}
 
 const Data = () => {
     const [data, setData] = useState(null);
 
-    d3.csv(url).then(data => {
-        let message = '';
-
-        /*message = message + `${Math.round(d3.csvFormat(data).length / 1024)} Kb`;
-        message += `${data.length} rows\n`;
-        message += `${data.columns.length} columns\n`*/ 
-    })
+    useEffect(() => {
+        console.log('Fetching data');
+        csv(url).then(setData);
+    }, []);
   
     return (
         <div className="App">
-            {/*message*/}
+            Data is { data ? Message(data) : 'loading'}
+
+            <hr/><br/>
+
+            <h2>Click on color name</h2>
+
+            { data ? data.map((el) => {
+
+                return <li 
+                            className = 'liItem' 
+                            key = {el.Keyword} 
+                            id = {el.Keyword}
+                            style = {liStyle} 
+                            onClick = {liClickHandler}
+                        >
+                              &#127754; {el.Keyword}
+                        </li>
+            }) : null}
         </div>
     );
 }
-
 export default Data;
