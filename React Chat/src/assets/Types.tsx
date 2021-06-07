c7e31b56b020b57649d28779677886d4dc46d3eb
Mit Sudebov <d.sudebov@telesputnik.ru>
2021-06-07 19:27:51 +0300

    Types. Some issue with message type. Fink about it. And cry...

--- React Chat/src/assets/Types.tsx
+++ React Chat/src/assets/Types.tsx
@@ -1,3 +1,51 @@
 export interface chatFeedProps{
-    
+    chats: [id: Chats];
+    activeChat: number;
+    userName: string;
+    messages: { id: undefined | ChatMessages };
 }
+
+export interface ChatMessages{
+    id: number;
+    sender: ChatsUsername;
+    created: string;
+    attachments: [];
+    sender_username: string;
+    text: string;
+    custom_json: {}
+}
+
+interface Chats{
+    access_key: string;
+    userName: ChatsUsername;
+    attachments: [];
+    created: string;
+    custom_json: {};
+    id: number;
+    is_authenticated: boolean;
+    is_direct_chat: boolean;
+    last_message: {
+        attachments: [];
+        created: string;
+        custom_json: null | {};
+        sender_username: string;
+        text: string;
+    };
+    people: [
+        {
+            chat_updated: string;
+            last_read: null | string;
+            person: ChatsUsername;
+        }
+    ];
+    title: string;
+}
+
+interface ChatsUsername{
+    avatar: string;
+    custom_json: {};
+    first_name: string;
+    is_online: boolean;
+    last_name: string;
+    username: string;
+};
